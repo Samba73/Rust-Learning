94a62f0df429572957d0f345a84c7ad8b40df59b
Samba <90577515+Samba73@users.noreply.github.com>
2023-12-23 15:40:08 +0530

    Update main.rs

--- struct/main.rs
+++ struct/main.rs
@@ -1,4 +1,48 @@
 #![allow(unused)]
+
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::io::{self, Read, Write};
+
+
+#[derive(Default, Debug)]
+struct EmployeeDetails {
+    first_name: String,
+    last_name: String,
+    dob: String,
+    dept: String,
+    mobile: String,
+    email: String,
+}
+
+fn read_employee_details() -> io::Result<EmployeeDetails>
+    {
+        let file = OpenOptions::new()
+                    .read(true)
+                    .open("employee.csv");
+        match file {
+            Ok(mut file) => {
+                let mut contents = String::new();
+                file.read_to_string(&mut contents)?;
+
+
+                let mut details = EmployeeDetails::default();
+
+                let fields: Vec<&str> = contents.split_whitespace().collect();
+
+                details.first_name = fields[0].to_string();
+                details.last_name = fields[1].to_string();
+                details.dob = fields[2].to_string();
+                details.dept = fields[3].to_string();
+                details.mobile = fields[4].to_string();
+                details.email = fields[5].to_string();
+
+                Ok(details)
+            }
+            Err(error) => Err(error),
+        }            
+    }                            
+
 #[derive(Debug)]
 struct Employee  {
     first_name: String,
@@ -24,23 +68,55 @@ impl Employee {
                 }
             }
 
-    fn add (&self) {
-        println!
-        ("The new employee {}, {} is added", 
-        self.first_name,self.last_name);
-    }        
+   fn write_employee_details(&self) {
+
+        let emp_str = format!(
+                     "{} {} {} {} {} {}\n",
+                    self.first_name, self.last_name, self.dob, 
+                    self.dept, self.mobile, self.email
+        );
+
+        let file = OpenOptions::new()
+                    .read(true)
+                    .write(true)
+                    .append(true)
+                    .create(true)
+                    .open("employee.csv");
+        match file {
+            Ok(mut file) => {
+                file.write_all(emp_str.as_bytes()).expect("Failed to write");
+            }
+            _ => {
+                println!("Failed to write in the file");
+            }
+        }            
+    }       
 
     fn display_employee (&self) {
-        println!("The newly added employee details are
-                  Name: {}, {}
-                  Date of Birth: {}
-                  Department: {}
-                  Mobile: {}
-                  Email: {}",
-                  self.first_name,self.last_name,
-                  self.dob, self.dept,
-                  self.mobile,self.email);
-    }
+        let emp = read_employee_details();
+       //println!("display emp {:#?}",emp);
+
+       match emp {
+        Ok(employeeeetails) => {                   
+            println!("The employee details are:
+                        Name: {}, {}
+                        Date of Birth: {}
+                        Department: {}
+                        Mobile: {}
+                        Email: {}", 
+                        employeeeetails.first_name, 
+                        employeeeetails.last_name,
+                        employeeeetails.dob, 
+                        employeeeetails.dept, 
+                        employeeeetails.mobile,
+                        employeeeetails.email)
+        }
+       
+       Err(_) => {
+        println!("Error Occured");
+       }
+       } 
+    }                
 }
 fn main() {
 
@@ -51,8 +127,9 @@ fn main() {
                                &String::from("9999999"), 
                                &String::from("samba@gmail.com"));
 
-    emp_1.add();
+    emp_1.write_employee_details();
     emp_1.display_employee();   
+
 
     let emp_2 = Employee::new("Raghu",
                                "Krishnamurthy",
@@ -61,7 +138,8 @@ fn main() {
                                "111111", 
                                "raghu.k@gmail.com");
 
-    emp_2.add();
-    emp_2.display_employee();                           
+    emp_2.write_employee_details();
+    emp_2.display_employee();   
+                    
 
 }
