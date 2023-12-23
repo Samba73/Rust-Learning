b2002f2cbae0499a94e0cfc5fc5b13e9241e0c12
Samba <90577515+Samba73@users.noreply.github.com>
2023-12-23 11:13:48 +0530

    Create main.rs

---
+++ struct/main.rs
@@ -0,0 +1,57 @@
+#![allow(unused)]
+#[derive(Debug)]
+struct Employee  {
+    first_name: String,
+    last_name: String,
+    dob: String,
+    dept: String,
+    mobile: String,
+    email: String,
+}
+
+impl Employee {
+    fn new(first_name: &str, last_name: &str,
+            dob: &str, dept: &str, mobile: &str,
+            email: &str) -> Employee 
+            {
+                Employee {
+                    first_name: first_name.to_string(),
+                    last_name: last_name.to_string(),
+                    dob: dob.to_string(),
+                    dept: dept.to_string(),
+                    mobile: mobile.to_string(),
+                    email: email.to_string(),
+                }
+            }
+
+    fn add (&self) {
+        println!
+        ("The new employee {}, {} is added", 
+        self.first_name,self.last_name);
+    }        
+
+    fn display_employee (&self) {
+        println!("The newly added employee details are
+                  Name: {}, {}
+                  Date of Birth: {}
+                  Department: {}
+                  Mobile: {}
+                  Email: {}",
+                  self.first_name,self.last_name,
+                  self.dob, self.dept,
+                  self.mobile,self.email);
+    }
+}
+fn main() {
+
+    let emp_1 = Employee::new(&String::from("Samba"),
+                               &String::from("Krish"),
+                               &String::from("10-Jul-1983"),
+                               &String::from("IT"),
+                               &String::from("9999999"), 
+                               &String::from("samba@gmail.com"));
+
+    emp_1.add();
+    emp_1.display_employee();                           
+
+}
